cc8ddcd397a19f1be0e3cb2b0b945c5ffd268bae
Galih Adhi Kusuma <noxturne.production@gmail.com>
2025-11-25 10:09:17 +0700

    fix tailwind

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tailwindcss";
 export default defineConfig({
   content: [
     "./index.html",
-    "./*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
